337b9ae3e22a89879ec78ce8aa49c3636df3622e
MarioChico <36939569+MarioChico@users.noreply.github.com>
2019-02-25 10:51:35 -0600

    T3_Mario

---
+++ CalculadoraWPF/CalculadoraWPF/Core/Calculator/ExpressionCalculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Data;
+
+
+namespace WpfCalculadoraBueno.Core.Calculator
+{
+    public class ExpressionCalculator : ICalculator
+    {
+
+        public double Calculate(string CalculateText)
+        {
+            var dataTable = new DataTable();
+            var value = dataTable.Compute(CalculateText, "");
+            return Convert.ToDouble(value);
+        }
+
+    }
+}
